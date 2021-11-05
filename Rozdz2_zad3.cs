da6268c076f8d76ffd0a27c113bc8732ba604ca5
urszulab <77877386+urszulab@users.noreply.github.com>
2021-11-05 18:17:17 +0100

    Update Rozdz2_zad3.cs

--- Rozdz2_zad3.cs
+++ Rozdz2_zad3.cs
@@ -1,4 +1,27 @@
 /* Napisz program, który oblicza wskaźnik masy ciała BMI. Program ma prosić 
 użytkownika o podanie wagi w kilogramach oraz wzrostu w metrach. Wzór: BMI = masa/wzrost*2  */
 
+using System;
+
+namespace Praca_dom1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double BMI, wzrost, masa;
+            Console.WriteLine("Podaj wzrost w metrach: ");
+            wzrost = double.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj mase w kg: ");
+            masa = double.Parse(Console.ReadLine());
+
+            BMI = masa / (wzrost * 2);
+            Console.WriteLine(" BMI wynosi {0:F2}", BMI); // :F2 zaokragli do 2 miejsc po przecinku
+            Console.ReadKey();
+        }
+        
+    }
+}
+//Urszula B.
+
 

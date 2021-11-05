4b2cd969a6865488fac2c66d85f7c53ec4f8989e
urszulab <77877386+urszulab@users.noreply.github.com>
2021-11-05 18:34:28 +0100

    Create Rozdz2_zad6.cs

---
+++ Rozdz2_zad6.cs
@@ -0,0 +1,12 @@
+/* Jaką wartość będzie miała zmienna x po wykonaniu poniższych instrukcji? Najpierw 
+oblicz wartości zmiennych, a dopiero później sprawdź wynik uruchamiając program. 
+int x, y = 4;
+x = (y -= 2);   //  x = ( y = y - 2) , gdzie     2  = (  2 =  4 - 2)
+x = y++;   // tu y wchodzi stare, czyli 2, x to wciaz 2
+x = y--;   // tu x o jeden wieksze, czyli 3, bo y sie zwiekszyl o 1 po wczesniej postinkrementacji
+
+// na koncu x to 3, a y to 2
+
+
+*/
+

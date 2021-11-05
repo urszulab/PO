23f7915025d52a4a548b7871fc4da19629d8fb3b
urszulab <77877386+urszulab@users.noreply.github.com>
2021-11-05 18:02:18 +0100

    Update Rozdz2_zad1.cs

--- Rozdz2_zad1.cs
+++ Rozdz2_zad1.cs
@@ -6,14 +6,11 @@ namespace Praca_dom1
         static void Main(string[] args)
         {
             double F, C;
-            Console.WriteLine("Podaj temp. w stopniach Celsjusza");//WriteLine() - printf
-            C = double.Parse(Console.ReadLine()); //ReadLine - scanf()
-                                                      //ReadLine();  wczytuje stringi, ale ze potrzebujemy liczby
-                                                      //TO METODA   double.Parse() zamieni na typ double
+            Console.WriteLine("Podaj temp. w stopniach Celsjusza");
+            C = double.Parse(Console.ReadLine()); 
             F = 32 + (9d / 5) * C;
             Console.WriteLine(F);
-            Console.ReadKey();
-           
+            Console.ReadKey(); 
         }
     }
 }

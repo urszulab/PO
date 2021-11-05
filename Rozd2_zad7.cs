2399f89dc4e85be6658b676b64751dba3e79c651
urszulab <77877386+urszulab@users.noreply.github.com>
2021-11-05 18:54:25 +0100

    Update Rozd2_zad7.cs

--- Rozd2_zad7.cs
+++ Rozd2_zad7.cs
@@ -1,8 +1,29 @@
 /* Co wyświetli się na ekranie po wykonaniu poniższych instrukcji. Najpierw oblicz 
 wartości zmiennych, a dopiero później sprawdź wynik uruchamiając program.
 int x, y = 5;
-x = ++y * 2;    // tu x to 12, bo preinkrementacja
-x = y++;    // tu x to 12 wciaz
-x = y--;      // tu x to 13, ale po wykonaniu insturkcji y to 12
+x = ++y * 2;   
+x = y++;   
+x = y--;     
 Console.WriteLine(++y);  */
-// ostatecznie x to 13, a y to 12, ale po wyswietleniu y bedzie o jeden wiekszy
+
+
+using System;
+
+namespace Praca_dom1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y = 5;
+            x = ++y * 2;   // x =12, y = 6
+            x = y++;  // x = 6,  po wykonaniu wyrazenia y = 7
+            x = y--; // x = 7,  po wykonaniu wyrazenia y =6
+            Console.WriteLine(++y); // przed wyswietleniem zwiekszenie o 1 y, wiec y=7 po wyswietleniu, bo preinkrementacja
+
+            Console.ReadKey();
+        }
+    }
+}
+
+//Urszula B.

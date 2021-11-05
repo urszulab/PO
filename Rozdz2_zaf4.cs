11578584f697da5144c4d8f8ca361f7e42712ec5
urszulab <77877386+urszulab@users.noreply.github.com>
2021-11-05 18:22:36 +0100

    Create Rozdz2_zaf4.cs

---
+++ Rozdz2_zaf4.cs
@@ -0,0 +1,7 @@
+/* Po wykonaniu poniższych linii programu:
+int x = 100;
+Console.WriteLine(++x * 2); */
+
+
+//wyswietli sie 202, bo jest preinkrementacja i przed wyswietleniem juz zostanie x zwiekszony o 1, wiec bedzie 101, potem zostanie wymnozone razy 2 i wyswietli finalnie 202.
+//Jesli by byl x++ zamiast ++x, to wyswietliloby 200, a dopiero potem zwiekszylo x o 1

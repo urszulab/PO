7353bfe7356753372e4d0bac9f6bae00246a3991
urszulab <77877386+urszulab@users.noreply.github.com>
2021-11-05 18:30:28 +0100

    Update Rozdz2_zad5.cs

--- Rozdz2_zad5.cs
+++ Rozdz2_zad5.cs
@@ -2,8 +2,8 @@
 int x = 2, y = 3;
  x *= y * 2; */
  
- 
- using System;
+
+using System;
 
 namespace Praca_dom1
 {
@@ -12,8 +12,12 @@ namespace Praca_dom1
         static void Main(string[] args)
         {
             int x = 2, y = 3;
-            x *= y * 2;    // to jest to samo co:  x = x * y * 2  , gdzie przypisywanie wartosci jest od prawej do lewej
+            x *= y * 2;   // to jest to samo co:  x = x * y * 2  , gdzie przypisywanie wartosci jest od prawej do lewej
             Console.WriteLine(" x={0}", x);
+            Console.ReadKey();
+
+
+
         }
     }
 }

04ed63e01ac43eac9e4bb18d83b87302e8bc4ff0
urszulab <77877386+urszulab@users.noreply.github.com>
2021-11-08 13:08:25 +0100

    Create Rozdz3_zad5.cs

---
+++ Rozdz3_zad5.cs
@@ -0,0 +1,49 @@
+/* Napisz program obliczający liczbę pierwiastków równania kwadratowego. Program ma 
+prosić użytkownika o podanie współczynników równania, a następnie ma wyświetlić 
+stosowny komunikat */
+
+using System;
+
+namespace Praca_dom1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Liczby typu double to z przecinkiem, nie z kropka np. 2,5   nie 2.5
+            double a, b, c;
+            Console.WriteLine("Podaj wspolczynnik a: ");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj wspolczynnik b: ");
+            b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj wspolczynnik c: ");
+            c = double.Parse(Console.ReadLine());
+            double delta = b * b - 4 * a * c;
+            double dzielnik = 2 * a;
+            if (delta > 0 && dzielnik != 0)
+            {
+                Console.WriteLine("Rownanie ma 2 pierwiastki");
+                Console.ReadKey();
+            }
+            else if (delta < 0 || dzielnik == 0)
+            {
+                if (delta < 0)
+                {
+                    Console.WriteLine("Brak rozwiazania");
+                    Console.ReadKey();
+                }
+                else Console.WriteLine("Zakaz dzielenia przez 0");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Rownanie ma 1 rozwiazanie");
+                Console.ReadKey();
+
+
+            }
+        }
+    }
+}
+
+//Urszula B.

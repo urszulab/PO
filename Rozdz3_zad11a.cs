0e72f7f7b2886741f9cc8de5375598c3293f0003
urszulab <77877386+urszulab@users.noreply.github.com>
2021-11-10 13:48:54 +0100

    Create Rozdz3_zad11a.cs

---
+++ Rozdz3_zad11a.cs
@@ -0,0 +1,34 @@
+/*Zadanie 3.11. 
+Napisz program wyswietlajacy :
+ 
+*
+**
+***
+****    
+
+ */
+
+using System;
+
+namespace rozdzial3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Podaj liczbe wierszy: ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++) // wiersz   
+            {
+                for (int j = 1; j <= i; j++) //kolumna
+                {
+                    if (j<=i) Console.Write("*"); 
+                    else Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }    
+    }
+}
+//Urszula B.
